f9e9bb62fb1f222c6bc9e2d09ff559a4972e6881
Romain <romain.pillot@epitech.eu>
2017-09-17 14:17:33 +0200

    explored logics

---
+++ logic.ex
@@ -0,0 +1,41 @@
+#cond, if/elseif statements
+
+IO.puts(if false do "never done" else "done" end)
+
+#with extra verbosity
+IO.puts(if false, do: "never done", else: "done")
+
+test = if true, do: (
+  a = 2 + 2
+  a = a + 2
+)
+
+if true do
+  IO.puts(test)
+end
+
+x = 10
+
+#used for if/elseif/else
+cond do
+  x == 10 ->
+    IO.puts("got ten")
+  x == 20 ->
+    IO.puts("lol")
+  true -> "else"
+end
+
+x = 20
+
+x = cond do x == 10 -> "got ten"; true -> "otherwise" end
+IO.puts(x)
+
+#case, pattern matching
+
+y = :tuple
+x = [:some, y, :values]
+
+case x do
+  x when hd(x) === y -> IO.puts("won't match, head of x is :some")
+  [:some, ^y, z] -> IO.puts("matched with z atom :#{z}")
+end
